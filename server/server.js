0eb23232dcecc2155f32bc52639bffb281969a63
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-05-04 21:48:26 +0100

    Added tests for /todos endpoint

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+
 
 
 const { User } = require('./models/user');
@@ -8,11 +8,15 @@ const { Todo } = require('./models/todo');
 const { mongoose } = require('./db/mongoose');
 const port = process.env.PORT || 3000;
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.post('/todos', (req, res) => {
+    console.log(req.body);
     const todo = new Todo({
         text: req.body.text
-    })
+    });
 
     todo.save().then((doc) => {
         console.log(doc);
