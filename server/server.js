19a33f2fd31a7d5fa8feedc2c899b6fca9b3c770
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-05-11 21:41:23 +0100

    Added Update endpoint

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb')
-
-
+const _ = require('lodash');
 
 const { User } = require('./models/user');
 const { Todo } = require('./models/todo');
@@ -52,6 +51,31 @@ app.get('/todos/:id', (req, res) => {
         });
 });
 
+app.patch('/todos/: id ', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({
+            message: "Id not Valid",
+        });
+    }
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send();
+    })
+});
 
 app.delete("/todos/:id", (req, res) => {
     var id = req.params.id;
@@ -71,6 +95,8 @@ app.delete("/todos/:id", (req, res) => {
             res.status(400).send();
         });
 });
+
+
 app.listen(port, () =>
     console.log(`Server is listening at http://localhost:${port}`)
 );
