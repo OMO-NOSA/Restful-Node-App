c89d71b4981293ab652870affe74722c11ab21c3
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-05-15 18:38:18 +0100

    Fixed multiple exports in server.js

--- server/server.js
+++ server/server.js
@@ -136,6 +136,3 @@ app.listen(port, () =>
     console.log(`Server is listening at http://localhost:${port}`)
 );
 module.exports = { app };
-module.exports = { app };
-module.exports = { app };
-module.exports = { app };
