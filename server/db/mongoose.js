00b82cafa6e0065df71b09df27d8e532831b4892
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-06-04 23:34:40 +0100

    Fixed Connection establishment between mongo and app container... Clear mongodb connection issues

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const url = "mongodb://localhost:27017/TodoApp2";
+const url = "mongodb://mongo:27017/TodoApp2";
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || url, {
